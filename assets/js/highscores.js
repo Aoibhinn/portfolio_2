8f76dd2bec1e2dc78b2df2f94bc36782790f3d10
Aoibhinn <97aoibhinn@gmail.com>
2021-12-05 16:36:34 +0000

    change high score to high scores

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,8 +1,9 @@
 const HIGH_SCORES_LIST = document.querySelector('#highScoresList');
-const HIGH_SCORE = JSON.parse(localStorage.getItem('highScore')) || [];
+const HIGH_SCORES = JSON.parse(localStorage.getItem('highScores')) || [];
 
 
-HIGH_SCORES_LIST.innerHTML = 
-HIGH_SCORE.map(score => {
+HIGH_SCORES_LIST.innerHTML = HIGH_SCORES
+
+.map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
-}).join('');
+}).join("");
