d25971b97f126133ce5ccd50a834e2d2b284f65e
Aoibhinn <97aoibhinn@gmail.com>
2021-12-05 22:21:34 +0000

    clean up console.logs, add highscores to localstorage and redirect back to home once score has been saved

--- assets/js/end.js
+++ assets/js/end.js
@@ -7,7 +7,6 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 5;
 
-console.log(highScores)
 
 finalScore.innerText = mostRecentScore;
 
@@ -17,20 +16,21 @@ username.addEventListener('keyup', () => {
 });
 
 saveHighScore = (e) => {
-    console.log("clicked the save button")
     e.preventDefault();
 
     const score = {
-        score: Math.floor(Math.random()*100),
+        score: mostRecentScore
         name: username.value
     };
     
     highScores.push(score);
 
-    highScores.sort( (a,b) => b.score - a.score )
+    highScores.sort( (a,b) => b.score - a.score );
 
     highScores.splice(5);
 
-    console.log(highScores)
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/')
+
    
 };
