405591d45b138e34af4be186fdb1fd89682922d2
Aoibhinn <97aoibhinn@gmail.com>
2021-12-06 20:22:16 +0000

    re do highscores.js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -2,4 +2,9 @@ const highScoresList = document.getElementById("highScoresList");
 //pulls array of highscores stored in local storage
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-console.log(highScores);
+highScoresList.innerHTML = highScores
+//displays highest scores from the array using the map method
+  .map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
