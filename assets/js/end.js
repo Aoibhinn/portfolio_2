9ec8663308ad679c712da7fb224e5bdd6de7dec1
Aoibhinn <97aoibhinn@gmail.com>
2021-12-08 11:25:24 +0000

    add comments to end.js file

--- assets/js/end.js
+++ assets/js/end.js
@@ -5,12 +5,13 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
+//sets the amount of high scores to be saved and displayed
 const MAX_HIGH_SCORES = 5;
 
 
 finalScore.innerText = mostRecentScore;
 
-
+//disables save score button if no name is input
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
@@ -23,10 +24,11 @@ saveHighScore = (e) => {
         name: username.value
     };
     
+    //push highscore into array
     highScores.push(score);
-
+    //sorts highscores into order
     highScores.sort( (a,b) => b.score - a.score );
-
+    //cuts the array of highscores to just show top 5
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
